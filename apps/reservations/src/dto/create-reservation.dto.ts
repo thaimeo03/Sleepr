6e9fa70bc365402cb82bf0687628cdc5969ed0bb
Thaimeo03 <thaitran15072003@gmail.com>
2023-11-14 23:24:26 +0700

    Logging and validation

--- apps/reservations/src/dto/create-reservation.dto.ts
+++ apps/reservations/src/dto/create-reservation.dto.ts
@@ -1,11 +1,20 @@
+import { Type } from 'class-transformer'
+import { IsDate, IsString, IsNotEmpty } from 'class-validator'
+
 export class CreateReservationDto {
+  @IsDate()
+  @Type(() => Date)
   startDate: Date
 
+  @IsDate()
+  @Type(() => Date)
   endDate: Date
 
-  userId: string
-
+  @IsString()
+  @IsNotEmpty()
   placeId: string
 
+  @IsString()
+  @IsNotEmpty()
   invoiceId: string
 }
--- apps/reservations/src/main.ts
+++ apps/reservations/src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core'
 import { ReservationsModules } from './reservations.module'
+import { Logger } from 'nestjs-pino'
+import { ValidationPipe } from '@nestjs/common'
 
 async function bootstrap() {
-  const app = await NestFactory.create(ReservationsModules)
+  const app = await NestFactory.create(ReservationsModules, { bufferLogs: true })
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }))
+  app.useLogger(app.get(Logger))
+
   await app.listen(3000)
 }
 bootstrap()
